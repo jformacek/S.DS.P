5e72884f309e2d4e49e2517e49fe2039a97cfeac
Jiri Formacek <jiri.formacek@hotmail.com>
2021-04-21 09:10:36 +0200

    bug fixes

--- S.DS.P.psm1
+++ S.DS.P.psm1
@@ -887,7 +887,7 @@ More about System.DirectoryServices.Protocols: http://msdn.microsoft.com/en-us/l
             
             if($null -ne $transform -and $null -ne $transform.OnSave) {
                 #transform defined -> transform to form accepted by directory
-                $attrVal = ,(& $transform.OnSave -Values $Object.($prop.Name))
+                $attrVal = @(& $transform.OnSave -Values $Object.($prop.Name))
             }
             else {
                 #no transform defined - take value as-is
@@ -1035,7 +1035,7 @@ More about System.DirectoryServices.Protocols: http://msdn.microsoft.com/en-us/l
 
             if($null -ne $transform -and $null -ne $transform.OnSave) {
                 #transform defined -> transform to form accepted by directory
-                $attrVal = ,(& $transform.OnSave -Values $Object.($prop.Name))
+                $attrVal = @(& $transform.OnSave -Values $Object.($prop.Name))
             }
             else {
                 #no transform defined - take value as-is
